2054ce6c54fa687653e293affbf9a76937ef907e
finrails <macacofinn@yahoo.com>
2022-06-04 20:43:59 -0400

    add: populate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CITY_NAMES = ["New York", "California", "Texas", "San Diego", "Dallas", "Detroit", "Washington"].freeze
+SHORT_CITY_NAMES = ["NYC", "CFC", "TXC", "SDC", "DLC", "DTC", "WSC"]
+
+CITY_NAMES.each_with_index do |city_name, index|
+  Airport.create(city: city_name, short_name: SHORT_CITY_NAMES[index])
+end
