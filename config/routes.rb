f1d568374911132da15d307c3af2446eed5e23c5
finrails <macacofinn@yahoo.com>
2022-06-16 14:26:25 -0400

    add: index, new resources to flight_books

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :flight_books, only: %i[ index new ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "flight_books#index"
