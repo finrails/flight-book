56a66f7923b2d6b88d95cb872de474f980d1fce9
finrails <macacofinn@yahoo.com>
2022-06-16 09:50:11 -0400

    upt: a dense view to some partials

--- app/controllers/flight_books_controller.rb
+++ app/controllers/flight_books_controller.rb
@@ -5,16 +5,6 @@ class FlightBooksController < ApplicationController
   def index
     @flight = Flight.new
 
-    if params[:flight]
-      @future_flights = Flight.where(departure_airport_id: params[:flight][:departure_airport], arrival_airport_id: params[:flight][:arrival_airport], start_datetime: params[:flight][:departure_time])
-
-      if @future_flights.exists?
-        render partial: 'future_flights', locals: { future_flights: @future_flights }
-      else
-        redirect_to root_path
-      end
-
-    end
-
+    @future_flights = Flight.where(departure_airport_id: params[:flight][:departure_airport], arrival_airport_id: params[:flight][:arrival_airport], start_datetime: params[:flight][:departure_time]) if params[:flight]
   end
 end
