7b1c1a86ba0a8ade96f80bd3d43c81b5c77ec375
finrails <macacofinn@yahoo.com>
2022-06-17 17:31:46 -0400

    remove: binding.pry useless line

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,11 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
-    binding.pry
     if @booking.save
+      flash[:notice] = "Congratulations! You have made a booking."
       redirect_to root_path
     else
+      flash[:alert] = "Wait... Something goes wrong, try do thats again."
       redirect_to flight_books_path
     end
   end
