dd714d829003144affe80ed4de31b615bf658d68
finrails <macacofinn@yahoo.com>
2022-06-17 16:02:36 -0400

    add: Bookings controller create action and bookins_params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    binding.pry
+    if @booking.save
+      redirect_to root_path
+    else
+      redirect_to flight_books_path
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
 end
