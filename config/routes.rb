b5ec3317d289f503f59ffacf5360d61a69ecc5c7
finrails <macacofinn@yahoo.com>
2022-06-17 16:00:35 -0400

    upt: resource create to bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :flight_books, only: %i[ index new ]
-  resources :bookings, only: %i[ new ]
+  resources :bookings, only: %i[ new create ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
